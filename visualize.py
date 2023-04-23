8cf610774f12c71cc073c10a74e6e6e0538a7bb1
Ben Ren <rensimiao.ben@gmail.com>
2023-04-23 14:54:10 -0400

    visualization update

--- visualize.py
+++ visualize.py
@@ -6,6 +6,8 @@ import cv2
 import pandas as pd
 import glob
 from tqdm import tqdm
+from multiprocessing import Pool
+import shutil
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -34,7 +36,7 @@ def get_bw_from_1d_mask(mask, reference_img):
 
 def plot_scatter(max_IoU_list, pixel_list,):
     # f = plt.figure(figsize=[5,5])
-    alpha = 0.005 if len(pixel_list) > 1e5 else 0.05
+    alpha = 0.01 if len(pixel_list) > 1e5 else 0.05
     # print(alpha)
     plt.scatter(max_IoU_list,pixel_list, alpha=alpha, s=5)
     plt.xlim([0, 1])
@@ -49,7 +51,8 @@ def visualize_single_mask(SAM_pred_folder,
                           pixel_list,
                           oracle_iou,
                           pixel_size,
-                          savefolder='sample_imgs'
+                          savefolder='sample_imgs',
+                          move_img=False,
                           ):
     """
     Visualize a 1x3 
@@ -58,8 +61,12 @@ def visualize_single_mask(SAM_pred_folder,
     # Get original img
     img_re = os.path.join(img_folder, 
                           img_prefix.replace('mask','').replace('gt_patch','img_patch')+'*')
+    if 'DG' in SAM_pred_folder or 'dg' in SAM_pred_folder:
+        img_re += '.jpg'
     img_name = glob.glob(img_re)[0]
     img = cv2.imread(img_name)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     # Get the current mask
     cur_mask_name = os.path.join(SAM_pred_folder, SAM_pred_mask_name)
     # print(cur_mask_name)
@@ -74,12 +81,31 @@ def visualize_single_mask(SAM_pred_folder,
     gt_mask_re = os.path.join(gt_mask_folder, img_prefix)
     # print(gt_mask_re)
     gt_mask_name = glob.glob(gt_mask_re + '*')
+    if 'DG' in SAM_pred_folder or 'dg' in SAM_pred_folder:
+        img_re += '.png'
     if len(gt_mask_name) == 0:
         gt_mask = np.zeros_like(img)[:, :, 0]
     else:
         gt_mask_name = gt_mask_name[0]
     gt_mask = cv2.imread(gt_mask_name)
-
+    if np.max(gt_mask) == 1:
+        gt_mask *= 255
+    if move_img:
+        gt_dest = os.path.join(savefolder, os.path.basename(gt_mask_name))
+        img_dest = os.path.join(savefolder, os.path.basename(img_name))
+        if not os.path.exists(gt_dest):
+            shutil.copyfile(gt_mask_name, gt_dest)
+        if not os.path.exists(img_dest):
+            shutil.copyfile(img_name, img_dest)
+        for i in range(3):
+            f = plt.figure()
+            cur_mask_bw = get_bw_from_1d_mask(cur_mask[i, :, :], img)
+            plt.imshow(cur_mask_bw)
+            plt.axis('off')
+            plt.savefig(os.path.join(savefolder, 
+                                     os.path.basename(cur_mask_name).replace('.','_mask_{}.'.format(i))))
+            plt.close('all')
+        return
     # Plotting function
     f = plt.figure(figsize=[15,10])
     ax = plt.subplot(231)
@@ -101,6 +127,7 @@ def visualize_single_mask(SAM_pred_folder,
     plt.savefig(os.path.join(savefolder, 
                              save_prefix + SAM_pred_mask_name.split('.')[0] + '.png'),
                 transparent=True, dpi=300)
+    plt.close('all')
 
 def visualize_solar(max_img=100):
     # First lets read the center prompt result.csv
@@ -127,9 +154,147 @@ def visualize_solar(max_img=100):
                             pixel_size=pixel_size,
                             savefolder='sample_imgs/solar_center'
                             )
+
+def visualize_inria_dg(max_img=1000, num_cpu=50, move_img=False):
+     # First lets read the center prompt result.csv
+    df = pd.read_csv('result_IOUs/inria_DG_center_object_wise_IOU.csv', 
+                     index_col=0)
     
+    scatter_arr = np.load('scatter_list/inria_DG_scatter_list.npy')
+    max_IoU_list, pixel_list = scatter_arr[:, 0], scatter_arr[:, 1]
+    
+    try: 
+        pool = Pool(num_cpu)
+        args_list = []
+        for i in tqdm(range(min(len(df), max_img))):
+            SAM_pred_mask_name = '{}_prompt_ind_{}'.format(df['img_name'].values[i].split('.')[0],
+                                                       df['prompt_ind'].values[i])
+            oracle_iou = max_IoU_list[i]
+            pixel_size = pixel_list[i]
+            args_list.append(('SAM_output/inria_DG_center_prompt_save',
+                            SAM_pred_mask_name,
+                            'datasets/Combined_Inria_DeepGlobe_650/patches',
+                            'datasets/Combined_Inria_DeepGlobe_650/patches',
+                            'test',
+                            max_IoU_list, 
+                            pixel_list,
+                            oracle_iou,
+                            pixel_size,
+                            'sample_imgs/inria_dg_center',
+                            move_img))
+        pool.starmap(visualize_single_mask, args_list)
+    finally:
+        pool.close()
+        pool.join()
+
+    # for i in tqdm(range(min(len(df), max_img))):
+    #     if df['prompt_ind'].values[i] > 5:      # Only output 5 masks per image
+    #         continue
+    #     SAM_pred_mask_name = '{}_prompt_ind_{}'.format(df['img_name'].values[i].split('.')[0],
+    #                                                    df['prompt_ind'].values[i])
+    #     oracle_iou = max_IoU_list[i]
+    #     pixel_size = pixel_list[i]
+
+    #     visualize_single_mask(SAM_pred_folder='SAM_output/inria_DG_center_prompt_save',
+    #                         SAM_pred_mask_name=SAM_pred_mask_name,
+    #                         gt_mask_folder='datasets/Combined_Inria_DeepGlobe_650/patches',
+    #                         img_folder='datasets/Combined_Inria_DeepGlobe_650/patches',
+    #                         save_prefix='test',
+    #                         max_IoU_list=max_IoU_list, 
+    #                         pixel_list=pixel_list,
+    #                         oracle_iou=oracle_iou,
+    #                         pixel_size=pixel_size,
+    #                         savefolder='sample_imgs/inria_dg_center'
+    #                         )
+
+def visualize_dg_road(max_img=1000, num_cpu=50,move_img=False):
+     # First lets read the center prompt result.csv
+    df = pd.read_csv('result_IOUs/DG_road_center_object_wise_IOU.csv', 
+                     index_col=0)
+    
+    scatter_arr = np.load('scatter_list/DG_road_scatter_list.npy')
+    max_IoU_list, pixel_list = scatter_arr[:, 0], scatter_arr[:, 1]
+
+
+
+    try: 
+        pool = Pool(num_cpu)
+        args_list = []
+        for i in tqdm(range(min(len(df), max_img))):
+            SAM_pred_mask_name = '{}_prompt_ind_{}'.format(df['img_name'].values[i].split('.')[0],
+                                                       df['prompt_ind'].values[i])
+            oracle_iou = max_IoU_list[i]
+            pixel_size = pixel_list[i]
+            args_list.append(('SAM_output/DG_road_center_prompt_save',
+                              SAM_pred_mask_name,
+                              'datasets/DG_road/train/',
+                              'datasets/DG_road/train/',
+                              'test',
+                              max_IoU_list,
+                              pixel_list,
+                              oracle_iou,
+                              pixel_size,
+                              'sample_imgs/dg_road_center',
+                              move_img))
+        pool.starmap(visualize_single_mask, args_list)
+    finally:
+        pool.close()
+        pool.join()
+
+            # for i in tqdm(range(min(len(df), max_img))):
+    #     SAM_pred_mask_name = '{}_prompt_ind_{}'.format(df['img_name'].values[i].split('.')[0],
+    #                                                    df['prompt_ind'].values[i])
+    #     oracle_iou = max_IoU_list[i]
+    #     pixel_size = pixel_list[i]
+
+        # visualize_single_mask(SAM_pred_folder='SAM_output/DG_road_center_prompt_save',
+        #                     SAM_pred_mask_name=SAM_pred_mask_name,
+        #                     gt_mask_folder='datasets/DG_road/train/',
+        #                     img_folder='datasets/DG_road/train/',
+        #                     save_prefix='test',
+        #                     max_IoU_list=max_IoU_list, 
+        #                     pixel_list=pixel_list,
+        #                     oracle_iou=oracle_iou,
+        #                     pixel_size=pixel_size,
+        #                     savefolder='sample_imgs/dg_road_center'
+        #                     )
+
+def visualize_cloud(max_img=1000):
+     # First lets read the center prompt result.csv
+    df = pd.read_csv('result_IOUs/cloud_center_object_wise_IOU.csv', 
+                     index_col=0)
+    
+    scatter_arr = np.load('scatter_list/cloud_scatter_list.npy')
+    max_IoU_list, pixel_list = scatter_arr[:, 0], scatter_arr[:, 1]
+
+    for i in tqdm(range(min(len(df), max_img))):
+        if df['prompt_ind'].values[i] > 5:      # Only output 5 masks per image
+            continue
+        SAM_pred_mask_name = '{}_prompt_ind_{}'.format(df['img_name'].values[i].split('.')[0],
+                                                       df['prompt_ind'].values[i])
+        oracle_iou = max_IoU_list[i]
+        pixel_size = pixel_list[i]
+
+        visualize_single_mask(SAM_pred_folder='SAM_output/cloud_center_prompt_save',
+                            SAM_pred_mask_name=SAM_pred_mask_name,
+                            gt_mask_folder='datasets/cloud/train_processed',
+                            img_folder='datasets/cloud/train_processed',
+                            save_prefix='test',
+                            max_IoU_list=max_IoU_list, 
+                            pixel_list=pixel_list,
+                            oracle_iou=oracle_iou,
+                            pixel_size=pixel_size,
+                            savefolder='sample_imgs/cloud_center'
+                            )
+
 if __name__ == '__main__':
-    visualize_solar()
+    # visualize_solar()
+    # visualize_inria_dg()
+    # visualize_dg_road()
+    # visualize_cloud()
+    # visualize_inria_dg(move_img=True)
+    visualize_dg_road(move_img=True)
+
     # visualize_single_mask(SAM_pred_folder='SAM_output/solar_pv_center_prompt_save',
     #                       SAM_pred_mask_name='11ska625755_23_13_prompt_ind_0__0.968635618686676_0.9178075790405273_0.7066956758499146.npy',
     #                       gt_mask_folder='datasets/solar_masks',
